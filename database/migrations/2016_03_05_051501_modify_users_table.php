c090a8ffd14818dbb463304bddd9954446f28a3e
Sahadat Hossain <sahadat5021@gmail.com>
2016-03-05 11:22:37 +0600

    modify user table. add role_id and status field

---
+++ database/migrations/2016_03_05_051501_modify_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('role_id')->after('remember_token');
+            $table->tinyInteger('status')->default(1)->after('role_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['role_id', 'status']);
+        });
+    }
+}
